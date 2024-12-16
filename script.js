19bca30615305cd6320b9f68a72fe3b2c847a86f
Miggi97G <m.famgarcia@outlook.com>
2024-12-16 14:33:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let myPizza = [
+    {
+    "name": "Pizza Margherita",
+    "price": 14.00,
+    "description": "Tomaten Mozzarella"
+},
+{
+    "name": "Pizza Diavola",
+    "price": 15.00,
+    "description": "Tomaten Mozzarella"
+},
+{
+    "name": "Pizza Tirolese",
+    "price": 16.00,
+    "description": "Tomaten Mozzarella"
+}
+]
+
+let myPasta = [
+    {
+    "name": "Pasta Carbonara",
+    "price": 14.00,
+    "description": "Tomaten Mozzarella"
+},
+{
+    "name": "Pasta",
+    "price": 15.00,
+    "description": "Tomaten Mozzarella"
+},
+{
+    "name": "Pasta",
+    "price": 16.00,
+    "description": "Tomaten Mozzarella"
+}
+]
+
+const pizza = document.getElementById("pizzaMenu");
+const pasta = document.getElementById("pastaMenu");
+
+myPizza.forEach(dish => {
+    
+  const li = document.createElement("li");
+  li.innerHTML = `<h3>${dish.name}</h3><p>${dish.description}</p><p>CHF ${dish.price.toFixed(2)}</p>`; 
+  pizza.appendChild(li);
+});
+
+myPasta.forEach(dish => {
+    const li = document.createElement("li");
+    li.innerHTML = `<h3>${dish.name}</h3><p>${dish.description}</p><p>CHF ${dish.price.toFixed(2)}</p>`; 
+    pasta.appendChild(li);
+  });
